05d99bf7c29635aad0aa2b1a8bf18d8c0f9672bd
Hasina18 <dudekulahasina58@gmail.com>
2025-03-22 16:33:34 +0530

    Initial commit: Added food delivery project

---
+++ delivery/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from .models import Restaurants,Menu
+class ResForm(forms.ModelForm):
+    class Meta:
+        model = Restaurants
+        fields = ['Res_name', 'Food_cat', 'rating', 'img', 'address']
+class MenuForm(forms.ModelForm):
+    class Meta:
+        model = Menu
+        fields = '__all__'
